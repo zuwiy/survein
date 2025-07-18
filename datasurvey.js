4e19096ddfd2532e235a09aa1b295e9e1dbc4f0c
schwartz <108350386+zuwiy@users.noreply.github.com>
2025-07-19 00:53:01 +0700

    Update datasurvey.js

--- datasurvey.js
+++ datasurvey.js
@@ -24,7 +24,7 @@ submit.addEventListener('click', function(e){
         pilihan10: ambilJawaban("pilihan10")
     };
 
-    fetch('https://script.google.com/macros/s/AKfycbwX.../exec', {
+    fetch('https://script.google.com/macros/s/AKfycbxX_2eyPECA7SD8XV-RHge6K-9F2v0Ua5AJPtrvWUIPhuF7AEyOtNtH1htmNrq7Nw6iyQ/exec', {
   method: 'POST',
   body: JSON.stringify({ name: 'Zuwiy' }),
   headers: {
